37b566b9cf24fa6055a661e2bcb85c866838a49d
WilsonElCrack <windsoldier0120@gmail.com>
2026-05-15 11:20:24 -0500

    feat: add create type for inventory

--- app/modules/inventory/api/routes.py
+++ app/modules/inventory/api/routes.py
@@ -3,7 +3,9 @@ from fastapi import APIRouter
 from app.core.db import SessionDep
 from app.modules.inventory.application.create_item_inventory import CreateItemInventory
 from app.modules.inventory.application.get_items_inventory import GetItemsInventory
-from app.modules.inventory.schemas.request import CreateItemRequest
+from app.modules.inventory.schemas.request import CreateItemRequest, CreateTypeInventoryRequest
+from app.modules.inventory.application.create_type_inventory import CreateTypeInventory
+
 
 router = APIRouter()
 
@@ -18,3 +20,9 @@ async def get_inventory(session: SessionDep):
 async def create_item(session: SessionDep, create_item_request: CreateItemRequest):
     create_item_app = CreateItemInventory(session=session)
     return await create_item_app.execute(create_item_request)
+
+
+@router.post("/types")
+async def create_type_inventory(session: SessionDep, create_type_request: CreateTypeInventoryRequest):
+    create_type_app = CreateTypeInventory(session=session)
+    return await create_type_app.execute(create_type_request)
