303174d19bdafb06362bec352d0ed67a49f4aaf0
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-15 10:21:35 -0500

    fix: set inventory route to /items

--- app/modules/inventory/api/routes.py
+++ app/modules/inventory/api/routes.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter
+
+from app.core.db import SessionDep
 from app.modules.inventory.api.domain.repositories import InventoryRepository
 from app.modules.inventory.api.domain.service import InventoryService
-from app.core.db import SessionDep
 
 router = APIRouter()
 
-@router.get("")
+
+@router.get("/items")
 async def get_inventory(session: SessionDep):
-    inventory_service = InventoryService(repository=InventoryRepository(session=session))
+    inventory_service = InventoryService(
+        repository=InventoryRepository(session=session)
+    )
     return await inventory_service.get_inventory()
