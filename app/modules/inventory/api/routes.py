2db945d9d66d50339f236dd7e07531c6d78124ce
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-15 10:54:09 -0500

    feat: add inventory item creation endpoint

--- app/modules/inventory/api/routes.py
+++ app/modules/inventory/api/routes.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.core.db import SessionDep
+from app.modules.inventory.application.create_item_inventory import CreateItemInventory
 from app.modules.inventory.application.get_items_inventory import GetItemsInventory
+from app.modules.inventory.schemas.request import CreateItemRequest
 
 router = APIRouter()
 
@@ -10,3 +12,9 @@ router = APIRouter()
 async def get_inventory(session: SessionDep):
     inventory_app = GetItemsInventory(session=session)
     return await inventory_app.execute()
+
+
+@router.post("/items")
+async def create_item(session: SessionDep, create_item_request: CreateItemRequest):
+    create_item_app = CreateItemInventory(session=session)
+    return await create_item_app.execute(create_item_request)
