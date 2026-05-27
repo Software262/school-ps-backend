1709038e22f924575f7f93f075c8742989091fe1
DiegoGR76 <diego.rodriguez.porti.7@gmail.com>
2026-05-27 14:16:50 -0500

    style: fix unused imports in observador table migration

--- alembic/versions/1efa10f067d4_feat_add_observador_table_for_module_9.py
+++ alembic/versions/1efa10f067d4_feat_add_observador_table_for_module_9.py
@@ -7,9 +7,6 @@ Create Date: 2026-05-27 10:15:07.988882
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-import sqlmodel
 
 
 # revision identifiers, used by Alembic.
