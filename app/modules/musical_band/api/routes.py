db8caac76b4d1fd8e0a09d604c6f9452868b2597
yeferson59 <116679774+yeferson59@users.noreply.github.com>
2026-05-27 00:05:51 -0500

    feat: add musical band items endpoint and service

--- app/modules/musical_band/api/routes.py
+++ app/modules/musical_band/api/routes.py
@@ -1,8 +1,26 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query
+
+from app.core.db import SessionDep
+from app.modules.musical_band.application.get_items_musical_band import (
+    GetItemsMusicalBand,
+)
+from app.modules.musical_band.schemas.request import FilterPaginationMusicalBand
+from app.shared.utils.response import Response
 
 router = APIRouter()
 
 
 @router.get("/items")
-async def get_all_instruments():
-    return []
+async def get_all_instruments(
+    session: SessionDep,
+    filter_pagination: Annotated[FilterPaginationMusicalBand, Query()],
+):
+    get_items_musicalBand = GetItemsMusicalBand(session=session)
+
+    data = await get_items_musicalBand.execute(filter_pagination=filter_pagination)
+
+    return Response(
+        data=data,
+    )
--- app/modules/musical_band/schemas/request.py
+++ app/modules/musical_band/schemas/request.py
@@ -1,1 +1,15 @@
+from typing import Literal
 
+from pydantic import Field, model_validator
+
+from app.modules.inventory.schemas.request import FilterPaginationInventory
+
+
+class FilterPaginationMusicalBand(FilterPaginationInventory):
+    item_type: Literal["banda", "deporte", "ajedrez"] | None = Field(default="banda")
+
+    @model_validator(mode="after")
+    def validate_modification_modes(self) -> "FilterPaginationMusicalBand":
+        if self.item_type != "banda":
+            raise ValueError("Invalido tipo para obtener los articulos")
+        return self
