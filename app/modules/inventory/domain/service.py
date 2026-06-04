e5c80067ead8aae45bc5190bff9efb8e521fbbe4
Wilson <windsoldier0120@gmail.com>
2026-06-03 22:26:01 -0500

    fix: fix get borrowing

--- app/modules/inventory/domain/service.py
+++ app/modules/inventory/domain/service.py
@@ -138,6 +138,9 @@ class InventoryService:
 
         type_id = await self.repository.get_type_id_by_name(filter_pagination.item_type)
 
+        if type_id is None:  
+            return []
+        
         return await self.repository.get_borrowings_pagination(
             offset=offset,
             limit=filter_pagination.limit,
