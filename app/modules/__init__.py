6af162383a71ffe606947dc58a5f078fabfb0933
DiegoGR76 <diego.rodriguez.porti.7@gmail.com>
2026-06-07 17:16:26 -0500

    fix: remover importaciones duplicadas de modulos en inicializador

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -10,11 +10,6 @@ from app.modules.health.api.routes import router as health
 from app.modules.inventory.api.routes import router as inventory
 from app.modules.musical_band.api.routes import router as musical_band
 from app.modules.principal.api.routes import router as principal
-from app.modules.tests.api.routes import router as tests
-from app.modules.tuition.api.routes import router as tuition
-from app.modules.classroom_holder.api.routes import router as classroom_holder
-from app.modules.cafeteria.api.routes import router as cafeteria
-from app.modules.chess.api.routes import router as chess
 from app.modules.sports.api.routes import router as sports
 from app.modules.tests.api.routes import router as tests
 from app.modules.training_schools.api.routes import router as training_schools
